27e85102ee2f00e4189a4518d5583889bbf2e259
MatheusHBMelo <matheushbmelo@gmail.com>
2023-09-13 09:51:56 -0300

    Added the findUserById method and support for exception handling.

--- src/main/java/br/com/matheushbmelo/workshopmongo/controllers/UserController.java
+++ src/main/java/br/com/matheushbmelo/workshopmongo/controllers/UserController.java
@@ -6,6 +6,7 @@ import br.com.matheushbmelo.workshopmongo.services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +26,11 @@ public class UserController {
         List<User> users = userService.findAllUsers();
         List<UserDto> usersDtos = users.stream().map(UserDto::new).toList();
         return ResponseEntity.status(HttpStatus.OK).body(usersDtos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> findById(@PathVariable(value = "id") String id){
+        User user = userService.findUserById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(new UserDto(user));
     }
 }
