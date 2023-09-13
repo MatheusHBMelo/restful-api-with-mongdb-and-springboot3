43ef65d7bf9e1c62b06cced3d6f53e5a88211198
MatheusHBMelo <matheushbmelo@gmail.com>
2023-09-13 09:50:52 -0300

    Added findById method

--- src/main/java/br/com/matheushbmelo/workshopmongo/services/UserService.java
+++ src/main/java/br/com/matheushbmelo/workshopmongo/services/UserService.java
@@ -2,9 +2,11 @@ package br.com.matheushbmelo.workshopmongo.services;
 
 import br.com.matheushbmelo.workshopmongo.domain.User;
 import br.com.matheushbmelo.workshopmongo.repositories.UserRepository;
+import br.com.matheushbmelo.workshopmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 
     public List<User> findAllUsers(){
         return userRepository.findAll();
+    }
+
+    public User findUserById(String id){
+        Optional<User> user = userRepository.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Nenhum objeto foi encontrado com o ID:" + id));
     }
 }
