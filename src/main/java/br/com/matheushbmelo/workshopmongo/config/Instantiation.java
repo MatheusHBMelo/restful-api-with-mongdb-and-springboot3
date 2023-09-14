dfdd40bbdfc51c4056344783766adda3fa06c3f9
MatheusHBMelo <matheushbmelo@gmail.com>
2023-09-14 09:42:47 -0300

    Refactoring the configuration class

--- src/main/java/br/com/matheushbmelo/workshopmongo/config/Instantiation.java
+++ src/main/java/br/com/matheushbmelo/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package br.com.matheushbmelo.workshopmongo.config;
 
 import br.com.matheushbmelo.workshopmongo.domain.Post;
 import br.com.matheushbmelo.workshopmongo.domain.User;
+import br.com.matheushbmelo.workshopmongo.domain.dto.AuthorDto;
 import br.com.matheushbmelo.workshopmongo.repositories.PostRepository;
 import br.com.matheushbmelo.workshopmongo.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -35,8 +36,8 @@ public class Instantiation implements CommandLineRunner {
         User u5 = new User(null, "Davi Jose", "davi@email.com");
         userRepository.saveAll(Arrays.asList(u1, u2, u3, u4, u5));
 
-        Post p1 = new Post(null, LocalDateTime.parse("21/07/2001 15:45:30", dtf), "Viagem para Recife", "Embarcando para a cidade de Recife", u4);
-        Post p2 = new Post(null, LocalDateTime.parse("18/03/2008 09:22:00", dtf), "Indo a escola", "Aula de quimica em breve!", u5);
+        Post p1 = new Post(null, LocalDateTime.parse("21/07/2001 15:45:30", dtf), "Viagem para Recife", "Embarcando para a cidade de Recife", new AuthorDto(u4));
+        Post p2 = new Post(null, LocalDateTime.parse("18/03/2008 09:22:00", dtf), "Indo a escola", "Aula de quimica em breve!", new AuthorDto(u5));
         postRepository.saveAll(Arrays.asList(p1, p2));
     }
 }
